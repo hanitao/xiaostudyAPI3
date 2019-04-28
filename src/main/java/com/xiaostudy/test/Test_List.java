21b7a65918c1b449f101fe97deb3f6f895ea2a8f
xiaostudy <19976335137@163.com>
2019-04-28 16:19:27 +0800

    commit 1

---
+++ src/main/java/com/xiaostudy/test/Test_List.java
@@ -0,0 +1,23 @@
+package com.xiaostudy.test;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: xiaostudy
+ * Date: 2019/4/27
+ * Time: 22:10
+ * Description: No Description
+ */
+public class Test_List {
+    public static void main(String[] args) {
+
+        ArrayList arrayList = new ArrayList();
+        System.out.println(arrayList.size() == 0);
+        System.out.println(arrayList.isEmpty());
+
+    }
+}
+
